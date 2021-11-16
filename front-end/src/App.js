58c05f79ef09705dfb2155d2b8f639009d18b6ea
Dillon Maks <dillon@Dillons-MacBook-Pro.local>
2021-11-16 17:04:56 -0600

    Added a cart

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Link, Route, Switch } from 'react-router-dom';
+import { useHistory } from 'react-router';
 
 // Components
 import Home from './components/Home.js';
@@ -10,12 +11,12 @@ import getCategories from './firebase/functions/getCategories.js';
 import ProductList from './components/ProductList.js';
 
 import navLogo from './assets/Makusu.png';
+import cart from './assets/cart.PNG';
 import './App.css';
-
-
 
 function App() {
   const [categories, setCategories] = useState([]);
+  const { push } = useHistory();
 
   useEffect(() => {
     getCategories()
@@ -23,6 +24,10 @@ function App() {
       setCategories(res);
     })
   }, [])
+
+  const handleCart = () => {
+    push('/cart');
+  }
 
   return (
     <div className="App">
@@ -40,10 +45,17 @@ function App() {
         </nav>
       </header>
 
+      <div className='cart' onClick={handleCart}>
+        <img src={cart}/>
+        {5 > 0 && <p>{10}</p>}
+      </div>
 
       <Switch>
         <Route path='/all'>
           <ProductList />
+        </Route>
+        <Route path='/cart'>
+          <h1>THIS IS YOUR CART</h1>
         </Route>
         <Route path='/product/:id'>
           <ItemPage />  
--- front-end/src/components/ItemPage.js
+++ front-end/src/components/ItemPage.js
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import getProduct from '../firebase/functions/getProduct';
 
+import './styles/ItemPage.css';
+
 
 const ItemPage = () => {
     const [item, setItem] = useState({})
@@ -15,9 +17,15 @@ const ItemPage = () => {
     }, [])
 
     return(
-        <div>
-            <img src={item.images[0]} />
-            <h1>{item.name}</h1>
+        <div className='itempage-container'>
+            <div className='center-items'>
+                <img src={item.images[0]} />
+            </div>
+            <div className='item-info'>
+                <h1>{item.name}</h1>
+                <h2>${item.price}</h2>
+                <p>{item.description}</p>
+            </div>
         </div>
     )
 }
